4d1e7410ae933baceca52c629e0c44b50729ab40
CejkaLuk <44003242+CejkaLuk@users.noreply.github.com>
2021-11-23 14:26:01 +0100

    Added health status calculation.

--- Matlab/covid_19_simulation.m
+++ Matlab/covid_19_simulation.m
@@ -8,28 +8,46 @@ num_days = 100;
 num_humans = 100;
 city_dimensions = [100; 100];
 show_everyday_plot = true;
+plot_paths = false;
+save_as_gif = false;
 
 
-%% Simulation             
-prague = city(city_dimensions, num_humans);
+%% Simulation
 
 progress_bar = waitbar(0,'1','Name','COVID-19 simulation progress',...
                        'CreateCancelBtn','setappdata(gcbf,''cancelling'',1)');
 
 sim_fig = figure('Name', 'COVID-19 Monte Carlo Simulation', ...
-                 'NumberTitle', 'off');
-             
-for k = 1:num_days
-    waitbar(k/num_days, progress_bar, ...
-            sprintf('%d/%d days', k, num_days));
+                 'NumberTitle', 'off', 'visible', 'off');
+
+filename = "simulation_gifs/simulation_" + string(floor(posixtime(datetime))) + ".gif";
+
+prague = city(city_dimensions, num_humans);
+prague.plot_simulation(0, ...
+                       plot_paths=plot_paths, ...
+                       fig=sim_fig, ...
+                       filename=filename, ...
+                       save_as_gif=save_as_gif);
+
+for day = 1:num_days
+    waitbar(day/num_days, progress_bar, ...
+            sprintf('%d/%d days', day, num_days));
     
     if getappdata(progress_bar, 'cancelling')
         break
     end
+
+    prague.simulate_day(day, ...
+                        show_plot=show_everyday_plot, ...
+                        plot_paths=plot_paths, ...
+                        fig=sim_fig, ...
+                        filename=filename, ...
+                        save_as_gif=save_as_gif);
     
-    prague.simulate_one_day(show_plot=show_everyday_plot);
 end
 
 delete(progress_bar);
 
-prague.plot_simulation();
+prague.plot_simulation(num_days, ...
+                       plot_paths=plot_paths, ...
+                       fig=sim_fig);
--- Matlab/population.m
+++ Matlab/population.m
@@ -4,16 +4,121 @@ classdef population < handle
     
     properties
         humans
+        humans_by_status
         size
     end
     
     methods
         function obj = population(num_humans)
             %POPULATION Construct a population.
-            %   Params:
-            %       - num_humans := number of humans in the population.
+            
+            arguments
+                num_humans (1, 1) {mustBeInteger, mustBeNonnegative};
+            end
             
             obj.size = num_humans;
+        end        
+        
+        function update_humans(obj)
+            %UPDATE_HUMANS Update the lists of humans by health status.
+            
+            obj.humans_by_status.susceptible = obj.get_susceptible_humans();
+            obj.humans_by_status.infected = obj.get_infected_humans();
+            obj.humans_by_status.recovered = obj.get_recovered_humans();
+        end
+        
+        function update_health_status(obj, day)
+            %UPDATE_HEALTH_STATUS Update the health status of the entire
+            % population on a given day.
+            
+            arguments
+                obj
+                day (1, 1) {mustBeInteger, mustBeNonnegative};
+            end
+
+            obj.update_infected(day);
+
+            obj.update_recovered(day);
+        end
+        
+        function update_infected(obj, day)
+            %UPDATE_INFECTED Update the infected portion of the population.
+            % In other words, update how the current infected will infect
+            % the susceptible.
+            
+            arguments
+                obj
+                day (1, 1) {mustBeInteger, mustBeNonnegative};
+            end
+            
+            for human = obj.humans_by_status.infected
+                obj.infect_susceptible_humans(day, human);
+            end
+        end
+        
+        function update_recovered(obj, day)
+            %UPDATE_RECOVERED Update the recovered portion of the population.
+            % In other words, update which of the infected are recovered on
+            % a given day.
+            
+            arguments
+                obj
+                day (1, 1) {mustBeInteger, mustBeNonnegative};
+            end
+            
+            for human = obj.humans_by_status.infected
+                human.check_recovered(day);
+            end
+        end
+        
+        function infect_susceptible_humans(obj, day, human)
+            %INFECT_SUSCEPTIBLE_HUMANS Calculate how a given human will
+            % infect susceptible humans on a given day.
+            
+            arguments
+                obj
+                day (1, 1) {mustBeInteger, mustBeNonnegative};
+                human (1, 1) {mustBeA(human, 'human')};
+            end
+            
+            for susceptible_human = obj.humans_by_status.susceptible                
+                infection_prob = human.get_infection_prob_of_human(susceptible_human);
+                
+                if rand() <= infection_prob
+                    susceptible_human.set_health_status("infected", ...
+                                                        day=day);
+                end
+            end                
+        end
+        
+        function infected_humans = get_infected_humans(obj)
+            %GET_INFECTED_HUMANS Return portion of humans that are
+            % infected.
+            
+            infected_humans = obj.get_humans_with_health_status("infected");
+        end
+        
+        function susceptible_humans = get_susceptible_humans(obj)
+            %GET_SUSCEPTIBLE_HUMANS Return portion of humans that are
+            % susceptible.
+            
+            susceptible_humans = obj.get_humans_with_health_status("susceptible");
+        end
+        
+        function recovered_humans = get_recovered_humans(obj)
+            %GET_RECOVERED_HUMANS Return portion of humans that are
+            % infected.
+            
+            recovered_humans = obj.get_humans_with_health_status("recovered");
+        end
+        
+        function humans = get_humans_with_health_status(obj, health_status)
+            %GET_HUMANS_WITH_HEALTH_STATUS Return humans with a specific
+            % health status
+            
+            humans_arr = [obj.humans{:}];
+            indices = arrayfun(@(h) h.health_status == health_status, humans_arr);
+            humans = vertcat(humans_arr(indices));
         end
     end
 end
